a8de7bef562bae9b8724cdb2a8d68d413ee45247
Efflam Castel <efflamm.castel@gmail.com>
2024-02-16 19:37:13 +0000

    euler 1, for range version

--- euler_1_python/euler_1.py
+++ euler_1_python/euler_1.py
@@ -2,10 +2,15 @@ import sys
 
 def euler_1(threshold):
   """Computes the sum of the numbers divisible by 3 or 5 below the threshold"""
-  pass
+  result = 0
+  for n in range(threshold):
+    if n % 3 == 0 or n % 5 == 0:
+      result += n
+  return result
+  
 
-assert euler_1(0), 0
-assert euler_1(10), 23
+assert euler_1(0) == 0
+assert euler_1(10) == 23, f'got {euler_1(10)} instead of 23'
 
 if __name__ == '__main__':
   print(euler_1(int(sys.argv[1])))
