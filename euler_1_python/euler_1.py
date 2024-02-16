038581b9addd4f916063e52d50b5b5324f0d3028
Efflam Castel <efflamm.castel@gmail.com>
2024-02-16 19:39:01 +0000

    euler 1, list comprehension style

--- euler_1_python/euler_1.py
+++ euler_1_python/euler_1.py
@@ -2,12 +2,7 @@ import sys
 
 def euler_1(threshold):
   """Computes the sum of the numbers divisible by 3 or 5 below the threshold"""
-  result = 0
-  for n in range(threshold):
-    if n % 3 == 0 or n % 5 == 0:
-      result += n
-  return result
-  
+  return sum([n for n in range(threshold) if n %3 == 0 or n % 5 == 0])
 
 assert euler_1(0) == 0
 assert euler_1(10) == 23, f'got {euler_1(10)} instead of 23'
